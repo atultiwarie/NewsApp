9f9e0cdb11279054b8c79b4b17eef752d62861dc
atultiwarie <atultiwari4767@gmail.com>
2025-07-26 09:16:22 +0530

    made responsive

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const API_KEY = "d312a1d401294daabcaeca984b885c8a";
-
 const newsContainer = document.querySelector("#news-container");
 const nav = document.querySelector(".nav");
+
+nav.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    const category = e.target.getAttribute("data-category");
+    fetchNews(category);
+  }
+});
 
 let fetchNews = async (category) => {
   try {
     const API_URL = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`;
     const res = await fetch(API_URL);
     const data = await res.json();
-    console.log(data);
     displayNews(data.articles)
   } catch (error) {
     console.error("Error to fetch news :", error);
@@ -17,12 +22,7 @@ let fetchNews = async (category) => {
 
 fetchNews("general");
 
-nav.addEventListener("click", (e) => {
-  if (e.target.tagName === "LI") {
-    const category = e.target.getAttribute("data-category");
-    fetchNews(category);
-  }
-});
+
 function displayNews(articles) {
   newsContainer.innerHTML = "";
   if (articles.length === 0) {
